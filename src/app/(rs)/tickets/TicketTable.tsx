421a0461fecccfe3d9756e58fca9ddf7e9fb7016
Umer Bajwa <umar2bajwa@gmail.com>
2025-07-23 21:49:09 +0500

    Add ticket table with filtering and sorting
    Introduces a TicketTable component using TanStack React Table for displaying, sorting, filtering, and paginating ticket search results. Adds reusable DebouncedInput and Filter components for column filtering. Updates ticket queries to include ticket id and completed status, and sorts results by creation date. Integrates the new table into the tickets page, replacing the previous JSON output.

---
+++ src/app/(rs)/tickets/TicketTable.tsx
@@ -0,0 +1,232 @@
+"use client"
+
+import type { TicketSearchResultsType } from "@/lib/queries/getTicketSearchResults"
+
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    ColumnFiltersState,
+    SortingState,
+    getPaginationRowModel,
+    getFilteredRowModel,
+    getFacetedUniqueValues,
+    getSortedRowModel,
+} from "@tanstack/react-table"
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+import {
+    CircleCheckIcon,
+    CircleXIcon,
+    ArrowUpDown,
+    ArrowDown,
+    ArrowUp,
+} from 'lucide-react'
+
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import Filter from "@/components/react-table/Filter"
+
+type Props = {
+    data: TicketSearchResultsType,
+}
+
+type RowType = TicketSearchResultsType[0]
+
+export default function TicketTable({ data }: Props) {
+    const router = useRouter()
+
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+
+    const [sorting, setSorting] = useState<SortingState>([
+        {
+            id: "ticketDate",
+            desc: false, // false for ascending 
+        }
+    ])
+
+    const columnHeadersArray: Array<keyof RowType> = [
+        "ticketDate",
+        "title",
+        "tech",
+        "firstName",
+        "lastName",
+        "email",
+        "completed",
+    ]
+
+    const columnHelper = createColumnHelper<RowType>()
+
+    const columns = columnHeadersArray.map((columnName) => {
+        return columnHelper.accessor((row) => { // transformational 
+            const value = row[columnName]
+            if (columnName === "ticketDate" && value instanceof Date) {
+                return value.toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                })
+            }
+            if (columnName === "completed") {
+                return value
+                    ? "COMPLETED"
+                    : "OPEN"
+            }
+            return value
+        }, {
+            id: columnName,
+            header: ({ column }) => {
+                return (
+                    <Button
+                        variant="ghost"
+                        className="pl-1 w-full flex justify-between"
+                        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    >
+                        {columnName[0].toUpperCase() + columnName.slice(1)}
+
+                        {column.getIsSorted() === "asc" && (
+                            <ArrowUp className="ml-2 h-4 w-4" />
+                        )}
+
+                        {column.getIsSorted() === "desc" && (
+                            <ArrowDown className="ml-2 h-4 w-4" />
+                        )}
+
+                        {column.getIsSorted() !== "desc" && column.getIsSorted() !== "asc" && (
+                            <ArrowUpDown className="ml-2 h-4 w-4" />
+                        )}
+                    </Button>
+                )
+            },
+            cell: ({ getValue }) => { // presentational 
+                const value = getValue()
+                if (columnName === "completed") {
+                    return (
+                        <div className="grid place-content-center">
+                            {value === "OPEN" ? <CircleXIcon className="opacity-25" /> : <CircleCheckIcon className="text-green-600" />}
+                        </div>
+                    )
+                }
+                return value
+            }
+        })
+    })
+
+    const table = useReactTable({
+        data,
+        columns,
+        state: {
+            sorting,
+            columnFilters,
+        },
+        initialState: {
+            pagination: {
+                pageSize: 10,
+            },
+        },
+        onColumnFiltersChange: setColumnFilters,
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        getFacetedUniqueValues: getFacetedUniqueValues(),
+        getSortedRowModel: getSortedRowModel(),
+    })
+
+    return (
+        <div className="mt-6 flex flex-col gap-4">
+            <div className="rounded-lg overflow-hidden border border-border">
+                <Table className="border">
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <TableHead key={header.id} className="bg-secondary p-1">
+                                        <div>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </div>
+                                        {header.column.getCanFilter() ? (
+                                            <div className="grid place-content-center">
+                                                <Filter column={header.column} />
+                                            </div>
+                                        ) : null}
+                                    </TableHead>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                className="cursor-pointer hover:bg-border/25 dark:hover:bg-ring/40"
+                                onClick={() => router.push(`/tickets/form?ticketId=${row.original.id}`)}
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id} className="border">
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext()
+                                        )}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex justify-between items-center">
+                <div className="flex basis-1/3 items-center">
+                    <p className="whitespace-nowrap font-bold">
+                        {`Page ${table.getState().pagination.pageIndex + 1} of ${table.getPageCount()}`}
+                        &nbsp;&nbsp;
+                        {`[${table.getFilteredRowModel().rows.length} ${table.getFilteredRowModel().rows.length !== 1 ? "total results" : "result"}]`}
+                    </p>
+                </div>
+                <div className="space-x-1">
+                    <Button
+                        variant="outline"
+                        onClick={() => table.resetSorting()}
+                    >
+                        Reset Sorting
+                    </Button>
+                    <Button
+                        variant="outline"
+                        onClick={() => table.resetColumnFilters()}
+                    >
+                        Reset Filters
+                    </Button>
+                    <Button
+                        variant="outline"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        Previous
+                    </Button>
+                    <Button
+                        variant="outline"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        Next
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,6 +1,7 @@
 import TicketSearch from "@/app/(rs)/tickets/TicketSearch"
 import { getOpenTickets } from "@/lib/queries/getOpenTickets"
 import { getTicketSearchResults } from "@/lib/queries/getTicketSearchResults"
+import TicketTable from "@/app/(rs)/tickets/TicketTable"
 
 export const metadata = {
     title: "Ticket Search",
@@ -18,7 +19,7 @@ export default async function Tickets({
         return (
             <>
                 <TicketSearch />
-                <p>{JSON.stringify(results)}</p>
+                {results.length ? <TicketTable data={results} /> : <p className="mt-4">No open tickets found</p>}
             </>
         )
     }
@@ -28,7 +29,7 @@ export default async function Tickets({
     return (
         <>
             <TicketSearch />
-            <p>{JSON.stringify(results)}</p>
+            {results.length ? <TicketTable data={results} /> : <p className="mt-4">No results found</p>}
         </>
     )
 }
